24953b5a9f4e56fe1a8911b0369e871f0c77276b
sherlin <sherlinrojas33@gmail.com>
2026-07-01 16:17:56 -0600

    fix router productos

--- Backend/urls.py
+++ Backend/urls.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from django.conf import settings
-from django.conf.urls.static import static
 
 from bibliotecaapp.views import (
     ProductoViewSet,
@@ -12,40 +10,14 @@ from bibliotecaapp.views import (
     comprar
 )
 
-# JWT IMPORT (FUERA de urlpatterns)
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-
-# ======================
-# ROUTER
-# ======================
 router = DefaultRouter()
 router.register(r'productos', ProductoViewSet)
 router.register(r'categorias', CategoriaViewSet)
 router.register(r'usuarios', UsuarioViewSet)
 router.register(r'pedidos', PedidoViewSet)
 
-# ======================
-# URLS PRINCIPALES
-# ======================
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    # API REST
     path('api/', include(router.urls)),
-
-    # LOGIN JWT
-    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
-    # COMPRA
     path('api/comprar/', comprar),
 ]
-
-# ======================
-# MEDIA (IMÁGENES)
-# ======================
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
