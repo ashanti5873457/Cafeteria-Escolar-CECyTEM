aac9a5ed25206c90703d5fa4683d5e9ca853b29b
sherlin <sherlinrojas33@gmail.com>
2026-07-01 15:45:12 -0600

    fix CORS

--- Backend/settings.py
+++ Backend/settings.py
@@ -120,7 +120,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    "https://cafeteria-escolar-cecytem-1.onrender.com",
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
