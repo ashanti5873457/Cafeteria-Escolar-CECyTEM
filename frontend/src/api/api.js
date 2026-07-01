4a9b6fb40ad5e0da3d43c6716636c05ed879819d
sherlin <sherlinrojas33@gmail.com>
2026-07-01 15:40:25 -0600

    fix: conectar backend escolar

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://cafeteria-online.onrender.com/api",
+  baseURL: "https://cafeteria-escolar-cecytem-1.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
