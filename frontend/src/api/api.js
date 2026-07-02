537a1648f6a3c7fc954e7e828e11592f3f54118c
sherlin <sherlinrojas33@gmail.com>
2026-07-01 19:50:20 -0600

    Corregir URL de la API

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,10 +1,48 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://cafeteria-escolar-cecy.onrender.com/api",
+  baseURL: "https://cafeteria-escolar-cecy.onrender.com/api/",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 export default API;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
